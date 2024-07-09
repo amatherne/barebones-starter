7bd377100acd62270d07fef593a67f5c0edb8522
austin-nine15 <pnkdrmmr@gmail.com>
2024-07-09 09:40:42 -0500

    Update client-page.tsx

--- app/websites/[...filename]/client-page.tsx
+++ app/websites/[...filename]/client-page.tsx
@@ -1,5 +1,6 @@
 "use client"
-import { useTina } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import { tinaField, useTina } from "tinacms/dist/react";
 import { WebsiteQuery } from "../../../tina/__generated__/types";
 
 interface ClientPageProps {
@@ -22,8 +23,11 @@ export default function Website(props : ClientPageProps) {
         <h1>
           {data.website.title}
         </h1>
-        <div>
+        {/*<div>
           {data.website.body}
+        </div>*/}
+        <div data-tina-field={tinaField(data.website, "body")}>
+          <TinaMarkdown content={content} />
         </div>
         <code>
           <pre
