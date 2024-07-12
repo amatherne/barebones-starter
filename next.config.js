a91bb32f20e8c1738fecdee5c477dbdffce08f2c
austin-nine15 <pnkdrmmr@gmail.com>
2024-07-12 17:50:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -24,5 +24,12 @@ module.exports = {
       },
     ];
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
 };
 
