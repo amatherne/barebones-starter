7a9bc316cbcbd58f1950cd97e5ab07d850aba4af
austin-nine15 <pnkdrmmr@gmail.com>
2024-07-12 20:35:03 -0500

    Update client-page.tsx

--- app/websites/client-page.tsx
+++ app/websites/client-page.tsx
@@ -1,6 +1,6 @@
 // ../app/websites/client-page.tsx
 
-import { Card } from '../../components/Utilities/Card';
+import { Card } from '../components/Utilities/Card';
 
 
 export default function PostList(props) {
