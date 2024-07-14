85d9af1b864cddd2250c972ba962c29d13d72b5c
amatherne <pnkdrmmr@gmail.com>
2024-07-14 14:05:22 -0500

    gallery refactor

--- app/[...filename]/client-page.tsx
+++ app/[...filename]/client-page.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 import { tinaField, useTina } from 'tinacms/dist/react';
 import { PageQuery } from '../../tina/__generated__/types';
+import Gallery from '../components/gallery';
 import ImgOutput from '../components/utilities/img';
 
 interface ClientPageProps {
@@ -31,46 +32,14 @@ const ClientPage = (props: ClientPageProps) => {
 
   // console.log(data);
 
-  const GallerySettings   = data.page.hero || null;
-  
-  const GalleryDef        = GallerySettings?.height || null;
-  const GalleryMin        = GallerySettings?.min_height || null;
-  const GalleryMax        = GallerySettings?.max_height || null;
-
-  const GalleryImages     = GallerySettings?.gallery || null;
-
-  const galleryStyle: React.CSSProperties = {
-    ...(GalleryDef && { '--height--default': GalleryDef.replace('%','vw') }),
-    ...(GalleryMin && { '--height--min': GalleryMin.replace('%','vw') }),
-    ...(GalleryMax && { '--height--max': GalleryMax.replace('%','vw') }),
-  };
-
-  const galleryClass = `
-    ${GalleryDef && GalleryMin && GalleryMax ? 'set-height--clamp' : ''}
-    ${GalleryDef && !GalleryMin && !GalleryMax ? 'set-height--default' : ''}
-    ${GalleryDef && GalleryMin && !GalleryMax ? 'set-height--min' : ''}
-    ${GalleryDef && !GalleryMin && GalleryMax ? 'set-height--max' : ''}
-  `.trim();
+  const gallerySettings = data.page.hero || null;
 
   // Check if the current page is not the home page to show the title
   const showTitle = props.params.filename.join("/") !== "home";
 
   return (
     <>
-      {GalleryImages && (
-        <div 
-          className={`gallery ${galleryClass ? 'set-height '+galleryClass : ''}`}
-          style={galleryStyle}
-        >
-          {GalleryImages.map((image, index) => (
-            image?.src ? (
-              <div key={index} className="gallery--item">
-                <ImgOutput src={image.src} alt={image.alt} className="gallery--image" />
-              </div>
-            ) : null
-          ))}
-        </div>
-      )}
+      {gallerySettings && <Gallery gallerySettings={gallerySettings} />}
 
       <section className="page page--default">
         <div className="page-width">
---
+++ app/components/gallery.tsx
@@ -0,0 +1,46 @@
+// ../components/gallery.tsx
+
+import React from 'react';
+import ImgOutput from './utilities/img';
+
+interface GalleryProps {
+  gallerySettings: {
+    height?: string;
+    min_height?: string;
+    max_height?: string;
+    gallery: Array<{ src: string; alt: string } | null>;
+  };
+}
+
+const Gallery: React.FC<GalleryProps> = ({ gallerySettings }) => {
+  const { height, min_height, max_height, gallery } = gallerySettings;
+
+  const galleryStyle: React.CSSProperties = {
+    ...(height && { '--height--default': height.replace('%', 'vw') }),
+    ...(min_height && { '--height--min': min_height.replace('%', 'vw') }),
+    ...(max_height && { '--height--max': max_height.replace('%', 'vw') }),
+  };
+
+  const galleryClass = `
+    ${height && min_height && max_height ? 'set-height--clamp' : ''}
+    ${height && !min_height && !max_height ? 'set-height--default' : ''}
+    ${height && min_height && !max_height ? 'set-height--min' : ''}
+    ${height && !min_height && max_height ? 'set-height--max' : ''}
+  `.trim();
+
+  return (
+    <div className={`gallery ${galleryClass ? 'set-height ' + galleryClass : ''}`} style={galleryStyle}>
+      {gallery.map((image, index) =>
+        image?.src ? (
+          <div key={index} className="gallery--item">
+            <ImgOutput src={image.src} alt={image.alt} className="gallery--image" />
+
+            
+          </div>
+        ) : null
+      )}
+    </div>
+  );
+};
+
+export default Gallery;
