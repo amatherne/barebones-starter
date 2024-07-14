c6e4ff7a59cff8e908f86f216372da638b3ff158
austin-nine15 <pnkdrmmr@gmail.com>
2024-07-14 00:32:26 -0500

    menu adjustments

--- utils/menu.js
+++ utils/menu.js
@@ -7,13 +7,20 @@ function useMenuHandler() {
 
     const Body              = document.body;
 
-    const MenuTriggers      = '.menu-trigger,.header--main-menu a';
+    const MenuTriggers      = '.menu-trigger,.header--main-menu a, .logo';
     const MenuOpenClass     = 'header-menu-is-open';
 
     const handleMenuToggle = (event) => {
+
+      // prevent the menu opening if you're clicking the logo, 
+      // but allow the logo to close menu if it already is open.
+      const isLogo = (event.target.classList.contains('logo') || event.target.parentNode.classList.contains('logo'));
+      if ( !Body.classList.contains(MenuOpenClass) && isLogo ) return;
+      
       if ( event.target.tagName === 'A' && event.target.getAttribute('href') === '#' ) {
         event.preventDefault();
       }
+
       Body.classList.toggle(MenuOpenClass);
     };
 
