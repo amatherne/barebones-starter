d2080853eadb0069ccad7b082cafbadbcc9c8362
amatherne <pnkdrmmr@gmail.com>
2024-07-20 02:16:54 -0500

    save

--- utils/importIcon.js
+++ utils/importIcon.js
@@ -3,7 +3,7 @@
 const importIcon = async (iconName) => {
   try {
     const url = `../app/components/icons/uploads/${iconName}.tsx`;
-    // console.log(url)
+    console.log(iconName)
     const icon = await import(`../app/components/icons/uploads/${iconName}.tsx`);
     // console.log(icon)
     // console.log(icon.default)
