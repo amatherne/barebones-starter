1ae71773832807131378ef78b95ad2e7cba66be3
amatherne <pnkdrmmr@gmail.com>
2024-07-21 08:45:23 -0500

    head update

--- app/components/head/head.tsx
+++ app/components/head/head.tsx
@@ -1,13 +1,10 @@
 // ../components/Head/Head.tsx
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
-import Globals from "../../../content/global_settings/global.json";
-
+import Globals from '../../../content/global_settings/global.json';
 import '../../../styles/global.scss';
-
-import '../../../utils/index.js'
-import { finishedLoading } from '../../../utils/index.js'
+import { finishedLoading } from '../../../utils/index.js'; 
 
 interface HeadProps {
   seoTitle?: string;
@@ -15,8 +12,21 @@ interface HeadProps {
 }
 
 const HeadElement: React.FC<HeadProps> = ({ seoTitle, seoText }) => {
+  useEffect(() => {
+    
+    // Microsoft Clarity
+    const clarityScript = document.createElement('script');
+    clarityScript.type = 'text/javascript';
+    clarityScript.async = true;
+    clarityScript.src = 'https://www.clarity.ms/tag/naukwulsi8';
+    document.head.appendChild(clarityScript);
 
-  finishedLoading();
+    return () => {
+      document.head.removeChild(clarityScript);
+    };
+  }, []);
+
+  finishedLoading(); 
 
   const globalSeoTitle = Globals.seo_title || '';
   const globalSeoText = Globals.seo_text || '';
@@ -30,26 +40,21 @@ const HeadElement: React.FC<HeadProps> = ({ seoTitle, seoText }) => {
     }[match] || match));
   };
 
-  const finalSeoTitle = seoTitle && seoTitle.length > 0 ? `${seoTitle} | ${globalSeoTitle}` : globalSeoTitle;
-  const finalSeoText = seoText && seoText.length > 0 ? seoText : globalSeoText;
+  const finalSeoTitle = seoTitle ? `${seoTitle} | ${globalSeoTitle}` : globalSeoTitle;
+  const finalSeoText = seoText ? seoText : globalSeoText;
 
   return (
-    <>
-      <head>
-        
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="theme-color" content="" />
-        
-        <title>{finalSeoTitle}</title>
-        <meta name="description" content={escapeHtml(finalSeoText)} />
-        
-        <link rel="stylesheet" href="https://use.typekit.net/akz3yrt.css" {...({ precedence: "default" } as any)} />
-      
-      </head>
-    </>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="theme-color" content="" />
+      <title>{finalSeoTitle}</title>
+      <meta name="description" content={escapeHtml(finalSeoText)} />
+      <link rel="stylesheet" href="https://use.typekit.net/akz3yrt.css" />
+    </Head>
   );
 };
 
 export default HeadElement;
+
