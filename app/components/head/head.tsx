d889a842e944b356bf5184c57768212fd28358c7
amatherne <pnkdrmmr@gmail.com>
2024-07-23 18:21:19 -0500

    clarity

--- app/components/head/head.tsx
+++ app/components/head/head.tsx
@@ -1,14 +1,16 @@
 // ../components/Head/Head.tsx
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
+import { clarity } from 'react-microsoft-clarity';
 import Globals from '../../../content/global_settings/global.json';
 
 import '../../../styles/global.scss';
 import '../../../styles/components.scss';
 import '../../../styles/sections.scss';
 
-import { finishedLoading } from '../../../utils/index.js'; 
+import { finishedLoading } from '../../../utils/index.js';
 
 interface HeadProps {
   seoTitle?: string;
@@ -17,10 +19,11 @@ interface HeadProps {
 
 const HeadElement: React.FC<HeadProps> = ({ seoTitle, seoText }) => {
 
-  finishedLoading(); 
+  finishedLoading();
 
-  const globalSeoTitle = Globals.seo_title || '';
-  const globalSeoText = Globals.seo_text || '';
+  const globalSeoTitle = Globals.seo_title || seoTitle || '';
+  const globalSeoText = Globals.seo_text || seoText || '';
+
 
   const escapeHtml = (unsafeText: string = ''): string => {
     return unsafeText.replace(/[&<"']/g, (match) => ({
@@ -34,33 +37,24 @@ const HeadElement: React.FC<HeadProps> = ({ seoTitle, seoText }) => {
   const finalSeoTitle = seoTitle ? `${seoTitle} | ${globalSeoTitle}` : globalSeoTitle;
   const finalSeoText = seoText ? seoText : globalSeoText;
 
+  useEffect(() => {
+    clarity.init('naukwulsi8');
+  }, []);
+
   return (
-    <Head>
-      
+    <>
+
+      <Head></Head>
+
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta name="theme-color" content="" />
       
       <title>{finalSeoTitle}</title>
-      <meta name="description" content={escapeHtml(finalSeoText)} />
+      <meta name="description" content={escapeHtml(finalSeoText)} />  
       
-      {/*<link rel="stylesheet" href="https://use.typekit.net/akz3yrt.css" />*/}
-{/*      <link rel="stylesheet" href="/_next/static/css/global.css" />
-      <link rel="stylesheet" href="/_next/static/css/components.css" />
-      <link rel="stylesheet" href="/_next/static/css/sections.css" />
-      */}
-      <script type="text/javascript">
-        {`
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "naukwulsi8");
-        `}
-      </script>
-
-    </Head>
+    </>
   );
 };
 
