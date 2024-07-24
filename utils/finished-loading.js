93fb121abf5f89bd3fe9a8b6a0579eb288fc284a
amatherne <pnkdrmmr@gmail.com>
2024-07-23 19:44:12 -0500

    darkmode test

--- utils/finished-loading.js
+++ utils/finished-loading.js
@@ -6,5 +6,17 @@ export function finishedLoading() {
   useEffect(() => {
     const Body = document.body;
     Body.classList.add('site-loaded');
+
+    if (isDarkMode) {
+      // Body.classList.add('dark-mode');
+      console.log('darkmode')
+    }
+
   }, []);
 }
+
+
+// Function to check if dark mode is enabled
+const isDarkMode = () => {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+};
