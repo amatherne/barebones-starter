a6a50aa671b8f622a323b31bd551737b58a900f5
amatherne <pnkdrmmr@gmail.com>
2024-07-25 10:46:43 -0500

    test

--- next.config.js
+++ next.config.js
@@ -6,16 +6,14 @@ const path = require('path');
 const es6Promise = require('es6-promise');
 const svgoConfig = require('./svgo.config');
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-// TODO: inspect bundles
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-
-// Polyfill ES6 Promise for older browsers
 es6Promise.polyfill();
 
 const nextConfig = {
-
   async rewrites() {
     return [
       {
@@ -29,21 +27,33 @@ const nextConfig = {
     ];
   },
 
+  webpack: (config, { isServer }) => {
 
-  webpack: (config, { isServer, defaultLoaders }) => {
-  //   // Disable HMR for both client and server builds
     if (!isServer) {
       config.resolve.alias['@sentry/node'] = '@sentry/browser';
+
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // Remove console logs
+            },
+            output: {
+              comments: false, // Remove comments
+            },
+          },
+          extractComments: false, // Disable extracting comments to separate files
+        })
+      );
     }
 
-    // Add SVGR for SVG handling
     config.module.rules.push({
       test: /\.svg$/,
       use: [
         {
           loader: '@svgr/webpack',
           options: {
-            svgo: {
+            svgoConfig: {
               plugins: [
                 { removeViewBox: false },
                 { removeDimensions: true },
@@ -55,36 +65,73 @@ const nextConfig = {
       ],
     });
 
-    // Add SCSS handling
-    // config.module.rules.push({
-    //   test: /\.scss$/,
-    //   use: [
-    //     'style-loader', // Creates `style` nodes from JS strings
-    //     'css-loader',   // Translates CSS into CommonJS
-    //     'sass-loader',  // Compiles Sass to CSS
-    //   ],
-    // });
-
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/,
       use: [
         {
           loader: 'file-loader',
           options: {
-            outputPath: 'static/fonts', // or any directory you prefer
-            publicPath: '/_next/static/fonts', // path in URL
+            outputPath: 'static/fonts', 
+            publicPath: '/_next/static/fonts', 
             name: '[name].[ext]',
           },
         },
       ],
     });
 
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    });
+
+    if (process.env.ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: isServer ? './bundles/server.html' : './bundles/client.html',
+        openAnalyzer: true,
+        analyzerPort: 8889, 
+      }));
+    }
+
+    config.optimization.minimizer.push(
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: ['default', {
+            discardComments: { removeAll: true }, // Remove all comments
+          }],
+        },
+      })
+    );
+
+    config.cache = {
+      type: 'filesystem', // Use filesystem cache
+      buildDependencies: {
+        config: [__filename], // Rebuild when this file changes
+      },
+    };
+
+    // config.optimization.usedExports = true;
+
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+      },
+      runtimeChunk: {
+        name: (entrypoint) => `runtime-${entrypoint.name}`,
+      },
+    };
+
     return config;
   },
-
-  // plugins: [
-  //   new BundleAnalyzerPlugin()
-  // ]
 };
 
 module.exports = nextConfig;
